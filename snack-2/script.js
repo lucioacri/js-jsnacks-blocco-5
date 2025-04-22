1feefe922b99fdca8b6020060f60ace346988297
lucioacri <lucioacri@gmail.com>
2025-04-22 13:37:09 +0200

    add snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,12 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
+
+const personName = people.map((person) => person.name);
+console.log(personName);
+
 // Risultato: 'Paolo', 'Giulia', 'Marco'
