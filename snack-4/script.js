a82efee6770d53589a081c07ab1fd4cae7ca95a9
lucioacri <lucioacri@gmail.com>
2025-04-22 13:45:49 +0200

    add snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,13 @@
 const posts = [
-  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
-  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
-  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
-  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+  { author: "Marco", date: "12/3/2021", content: "lorem ipsum..." },
+  { author: "Luca", date: "30/6/2021", content: "lorem ipsum..." },
+  { author: "Fabrizio", date: "12/9/2020", content: "lorem ipsum..." },
+  { author: "Enrico", date: "09/12/2021", content: "lorem ipsum..." },
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
+
+const authorNames = posts.map((person) => person.author);
+console.log(authorNames);
+
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
