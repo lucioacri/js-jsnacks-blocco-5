994c3bc1513f046da3d472a7434c421edadbd3d0
lucioacri <lucioacri@gmail.com>
2025-04-22 13:34:52 +0200

    add snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
+
+const arrayNames = names.map((name) => name);
+console.log(arrayNames);
+
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
